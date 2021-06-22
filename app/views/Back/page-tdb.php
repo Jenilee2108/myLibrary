aa6eb3a073f207ef90c948af0078174a0220c555
Jenilee2108 <jenilee.mb@gmail.com>
2021-06-22 19:12:26 +0200

    probleme de requete commentaire 3

--- app/views/Back/page-tdb.php
+++ app/views/Back/page-tdb.php
@@ -33,9 +33,10 @@ $titre = "Mon tableau de bord";
 <!-- on boucle sur chaque commentaire associé au pseudo -->
 <section class="container livreCommente">
     <!-- On affiche les données présentes en base de données -->
-    <?php foreach ($allComms as $comm) : 
-        var_dump($pseudo);
+    <?php 
         var_dump($allComms);
+        var_dump($pseudo);die;
+    foreach ($allComms as $comm) : 
         ?>
         <div class="card-livre">
             <!-- titre du livre -->
