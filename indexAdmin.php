189af5e9ecd95f8d0884a58e5e1c1b721663b8b7
Jenilee2108 <jenilee.mb@gmail.com>
2021-06-22 05:42:45 +0200

    modification indexAdmin 4

--- indexAdmin.php
+++ indexAdmin.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-
+use Dotenv\Repository\Adapter\EnvConstAdapter;
 @session_start();
 @$pseudo = $_SESSION['user']['pseudo'];
 var_dump($_SESSION);
